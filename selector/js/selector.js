d5bda0513117522965448b29b291f6d5b0a4b06b
AngelaDuoduo <dxshatangju@foxmail.com>
2014-05-28 09:37:34 +0800

    Update selector.js

--- selector/js/selector.js
+++ selector/js/selector.js
@@ -1,5 +1,6 @@
 main();
 
+//主函数，获取页面上所有.selector，依次进行样式设置及事件绑定。
 function main() {
 	
 	var selectors = $(".selector");
@@ -12,60 +13,83 @@ function main() {
 	
 }
 
+//设置.selecor样式
 function createSelector(selector, name) {
 	
+	//隐藏选项列表
 	var options = selector.getElementsByTagName("div");
 	for (var i = 0; i < options.length; i++) {
 		addClass(options[i], "option hide");
 	}	
 	
+	//选择框容器
 	var inputContainer = document.createElement("div");
 	addClass(inputContainer, "input_container");	
 	
+	//隐藏input, 用于表单提交
 	var formData = document.createElement("input");
 	formData.setAttribute("type", "hidden");
 	if (name) {	
 		formData.setAttribute("name",name);
 	}	
 	
+	//选择输入框
 	var input = document.createElement("div");	
 	addClass(input, "selector_input");		
 	
+	//选择下拉箭头
 	var button = document.createElement("div");
 	addClass(button, "sword");
 	
+	//将input, 输入框及箭头加入容器
 	inputContainer.appendChild(formData);
 	inputContainer.appendChild(input);
 	inputContainer.appendChild(button);
 	selector.insertBefore(inputContainer, selector.firstChild);
 	
+	//设置列表样式
 	buildList(selector);
 	
 }
 
 function buildList(selector, options) {
 	
+	//创建列表框，添加样式，然后隐藏
 	var choiceList = document.createElement("div");
 	addClass(choiceList, "choice_list hide");
+	
+	//获取selector下所有选项<div>元素
 	var options = getClassChilds(selector, "option");
 	var len = options.length;
 	
+	//将选项元素加入列表框，移除hide类，只保留列表框隐藏
 	for (var i = 0; i < len; i++) {
 		choiceList.appendChild(options[i]);
 		removeClass(options[i], "hide");
 		
-	}		
+	}
+	
+	//将列表框加入selector末尾
 	selector.appendChild(choiceList);
 }
 
+//设置selector被点击时的操作
 function addChooseListenerForSelector(selector) {
 
+	//得到选择框容器
 	var input = getClassChilds(selector, "input_container");
 	var target = input[0];
+	
+	//得到选择列表
 	var list = getClassChilds(selector, "choice_list")[0];
+	
+	//得到选择输入框
 	var value_show_box = getClassChilds(target, "selector_input")[0];
+	
+	//得到隐藏的input元素
 	var form_input = target.getElementsByTagName("input")[0];
 	
+	//为选择框容器click事件绑定操作，点击选择输入框或箭头均有效。先点击显示，后点击隐藏。
 	target.addEventListener("click", function(event) {
 	
 		if (!hasClass(list, "hide")) {
@@ -80,11 +104,17 @@ function addChooseListenerForSelector(selector) {
 	
 	});
 	
+	//为列表框click事件绑定操作
 	list.addEventListener("click", function(event) {
 	
+		//获得选项内容，并令其显示在选择输入框中。
 		var choice = event.target.innerText;
 		value_show_box.innerText = choice;
+		
+		//设置input元素value属性
 		form_input.setAttribute("value", choice);
+		
+		//隐藏列表
 		removeClass(list, "fadeIn");
 		addClass(list, "hide");
 	
@@ -94,7 +124,7 @@ function addChooseListenerForSelector(selector) {
 
 
 
-
+//id及class选择器
 function $(selector) {
 	var elems = new Array();
 	if (/^#/.test(selector)) {
@@ -116,6 +146,7 @@ function $(selector) {
 	return elems;
 }
 
+//得到elem元素下class属性中包含值className的子元素数组
 function getClassChilds(elem, className) {
 
 	var childs = elem.childNodes;
@@ -133,6 +164,7 @@ function getClassChilds(elem, className) {
 	return classChilds;
 }
 
+//判断elem元素的class属性的值是否包含oneClass值
 function hasClass(elem, oneClass) {
 	if(!elem.hasAttribute("class")) return false;
 	var className = " " + elem.getAttribute("class") + " ";
@@ -140,6 +172,8 @@ function hasClass(elem, oneClass) {
     return className.indexOf(oneClass) != -1;	
 }
 
+
+//判断elem元素的class属性的值是否包含classSet中所有值
 function hasClassSet(elem, classSet) {
 
 	if(!elem.hasAttribute("class")) return false;
@@ -155,6 +189,8 @@ function hasClassSet(elem, classSet) {
 	return true;
 
 }
+
+//为elem元素class属性添加值newClass
 function addClass(elem, newClass) {
 	var className;
 	if (elem.hasAttribute("class")) {
@@ -171,6 +207,7 @@ function addClass(elem, newClass) {
 	}
 }
 
+//将targetClass在elem元素class属性的值中移除
 function removeClass(elem, targetClass) {
 
 	if(elem.hasAttribute("class")) {
